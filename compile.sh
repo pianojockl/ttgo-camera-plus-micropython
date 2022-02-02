c7f435764577a53712e84a57988511f0354129b3
Joachim Werner <jockl@pianojockl.org>
2022-02-02 13:22:13 +0100

    initial commit

---
+++ compile.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# prepare esp-idf
+cd esp-idf
+git checkout v4.2
+./install.sh esp32
+source export.sh
+cd ..
+
+# checkout micropython version v1.18
+cd micropython
+git checkout v1.18
+cd ..
+
+# checkout the right version of lemariva's camera driver and copy board definition into micropython folder structure
+cd modules/micropython-camera-driver
+git checkout 83051e2130791a26781559b851e7f717eda5a752
+cd ../..
+
+cp -v -r modules/micropython-camera-driver/boards/* micropython/ports/esp32/boards/
+
+# checkout the right version of ross hughes' st7789 driver module
+cd modules/st7789_mpy
+git checkout eeaaa927bc8419e94ced95b27a6f2d6285dc86b8
+cd ../..
+
+# compile micropython
+cd micropython/ports/esp32
+make BOARD=GENERIC_CAM clean && make USER_C_MODULES=../../../../modules/micropython.cmake BOARD=GENERIC_CAM all
---
+++ modules/micropython.cmake
@@ -0,0 +1,2 @@
+include(${CMAKE_CURRENT_LIST_DIR}/micropython-camera-driver/src/micropython.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/st7789_mpy/st7789/micropython.cmake)
