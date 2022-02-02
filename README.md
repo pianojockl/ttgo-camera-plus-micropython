98241b34290e677d2dd57b5d5da4dbd0b575f6e4
Joachim Werner <jockl@pianojockl.org>
2022-02-02 13:58:31 +0100

    working state

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # ttgo-camera-plus-micropython
-Instructions for compiling micropython for the ttgo camera plus board
+This repo should contain everything to compile micropython for the ttgo camera plus board.
+The cam driver comes from lemariva: https://github.com/lemariva/micropython-camera-driver
+ST7789 display driver is from Ross Hughes: https://github.com/russhughes/st7789_mpy
+Micropython version is 1.18, esp-idf version is 4.2
 
 It really is pretty simple:
 
 git checkout --recursive https://github.com/pianojockl/ttgo-camera-plus-micropython
 
+cd ttgo-camera-plus-micropython
+
 ./do-everything.sh
+
+Good luck!
--- do-everything.sh
+++ do-everything.sh
@@ -2,7 +2,6 @@
 
 # prepare esp-idf
 cd esp-idf
-git checkout v4.2
 ./install.sh esp32
 source export.sh
 cd ..
@@ -11,23 +10,11 @@ cd ..
 mkdir esp-idf/components/esp32-camera
 cp -v -r esp-idf-components-esp32-camera/* esp-idf/components/esp32-camera
 
-# checkout micropython version v1.18
-cd micropython
-git checkout v1.18
-cd ..
-
-# checkout the right version of lemariva's camera driver and copy board definition into micropython folder structure
-cd modules/micropython-camera-driver
-git checkout 83051e2130791a26781559b851e7f717eda5a752
-cd ../..
-
+# copy board definition of lemariva's camera driver into micropython folder structure
 cp -v -r modules/micropython-camera-driver/boards/* micropython/ports/esp32/boards/
-
-# checkout the right version of ross hughes' st7789 driver module
-cd modules/st7789_mpy
-git checkout eeaaa927bc8419e94ced95b27a6f2d6285dc86b8
-cd ../..
 
 # compile micropython
 cd micropython/ports/esp32
 make BOARD=GENERIC_CAM clean && make USER_C_MODULES=../../../../modules/micropython.cmake BOARD=GENERIC_CAM all
+
+echo If everything went well, your firmware.bin should be in micropython/ports/esp32/build-GENERIC_CAM/
